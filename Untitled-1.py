cbbebb5beb6cc7f615d9b990f71c9cf4b399f845
MiriamZer <miriam.zeresenai@ucalgary.ca>
2025-02-06 14:28:42 -0700

    updated uml

---
+++ Untitled-1.py
@@ -0,0 +1,19 @@
+'''
+This dataset contains various country-level world statistics, including the rate
+of internet usage and the per- person income.
+• Produce a Python program which separates the dataset in two groups, one
+with countries with income below 10,000, and one with countries with income
+at or above 10,000
+• Then, extend it to produce two histogram (distribution) plots depicting internet
+usage, one for the first group of countries and one for the second group of
+countries
+• The output files should be called hist1.png and hist2.png
+'''
+import matplotlib.pyplot as plt
+
+plt.figure(1)
+plt.figure(2)
+plt.xlabe;('')
+plt.ylabel('Countries')
+plt.show()
+print("hello")
